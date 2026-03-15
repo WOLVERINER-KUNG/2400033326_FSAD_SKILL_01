764b92a104b9807cb51b11f63609cc95033129f6
KAVATI JOHN SHREYAN <johnshreyannicky19@gmail.com>
2026-03-15 12:33:17 +0530

    Add files via upload

---
+++ src/main/java/klu/Spring_Boot_Demo/Main.java
@@ -0,0 +1,8 @@
+package klu.Spring_Boot_Demo;
+
+public class Main {       
+public static void main(String[] args)  
+{  
+System.out.println("Hello Git");  
+}  
+}
---
+++ src/test/java/klu/Spring_Boot_Demo/Skill1ApplicationTests.java
@@ -0,0 +1,13 @@
+package klu.Spring_Boot_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skill1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
